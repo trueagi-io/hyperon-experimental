c32da263a7781697753fbd9a1595ed6cd0a2f432
Vitaly Bogdanov <vsbogd@gmail.com>
2021-09-28 20:00:58 +0300

    Add unit tests for the GroundedAtom implementation for Operation struct

--- rust/src/common.rs
+++ rust/src/common.rs
@@ -30,3 +30,40 @@ impl Display for &'static Operation {
         write!(f, "{}", self.name)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test(_ops: &mut Vec<Atom>, _data: &mut Vec<Atom>) -> Result<(), String> {
+        Ok(())
+    }
+
+    #[test]
+    fn test_operation_display() {
+        let op = &Operation{ name: "test", execute: test };
+        assert_eq!(format!("{}", Atom::gnd(op)), "test");
+    }
+
+    #[test]
+    fn test_operation_eq() {
+        let a = Atom::gnd(&Operation{ name: "a", execute: test });
+        let aa = Atom::gnd(&Operation{ name: "a", execute: test });
+        let b = Atom::gnd(&Operation{ name: "b", execute: test });
+        assert!(a == aa);
+        assert!(a != b);
+    }
+
+    #[test]
+    fn test_operation_clone() {
+        let opa = Atom::gnd(&Operation{ name: "a", execute: test });
+        let opc = opa.clone();
+        if let (Atom::Grounded(boxa), Atom::Grounded(boxc)) = (opa, opc) {
+            let ptra: *const Operation = *(boxa.downcast::<&Operation>().unwrap());
+            let ptrc: *const Operation = *(boxc.downcast::<&Operation>().unwrap());
+            assert_eq!(ptra, ptrc);
+        } else {
+            assert!(false);
+        }
+    }
+}
