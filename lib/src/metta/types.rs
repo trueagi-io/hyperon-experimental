f4542ab63691c614a9f6667a045df76f3643d8e9
Alexey Potapov <pas.aicv@gmail.com>
2022-03-22 21:58:03 +0300

    Some simple dependent types checks

--- lib/src/metta/types.rs
+++ lib/src/metta/types.rs
@@ -316,6 +316,43 @@ mod tests {
     }
 
     #[test]
+    fn simple_dep_types() {
+        init_logger();
+        let space = metta_space("
+            (: = (-> $t $t Prop))
+            (: Entity Prop)
+            (: Human (-> Entity Prop))
+            (: Socrates Entity)
+            (: (Human Socrates) Prop)
+            (: Plato Entity)
+            (: Mortal (-> Entity Prop))
+            (: HumansAreMortal (-> Prop Prop))
+            (= (HumansAreMortal (Human $x)) (Mortal $x))
+            (: Time NotEntity)
+            (: SocratesIsHuman Prop)
+        ");
+        let t = &AtomType::Specific(atom("Prop"));
+        assert!(check_type(&space, &atom("(Human Socrates)"), t));
+        assert!(check_type(&space, &atom("(Human Plato)"), t));
+        assert!(!check_type(&space, &atom("(Human Time)"), t));
+        assert!(!validate_atom(&space, &atom("(Human Time)")));
+        assert!(check_type(&space, &atom("(= Socrates Socrates)"), t));
+        assert!(check_type(&space, &atom("(= Socrates Plato)"), t));
+        assert!(check_type(&space, &atom("(= Socrates Untyped)"), t)); //?
+        assert!(!check_type(&space, &atom("(= Socrates Time)"), t));
+        assert!(validate_atom(&space, &atom("(HumansAreMortal (Human Socrates))")));
+        assert!(validate_atom(&space, &atom("(HumansAreMortal (Human Plato))")));
+        assert!(!validate_atom(&space, &atom("(HumansAreMortal (Human Time))")));
+        assert!(!validate_atom(&space, &atom("(HumansAreMortal Human)")));
+        //assert!(check_type(&space, &atom("(HumansAreMortal (Human Socrates))"),
+        //                   &AtomType::Specific(atom("(Mortal Socrates)"))));
+        //assert!(validate_atom(&space, &atom("(= SocratesIsHuman (Human Socrates))")));
+        //assert!(validate_atom(&space, &atom("(= SocratesIsHuman (Human Plato))")));
+        //assert!(check_type(&space, &atom("(= SocratesIsHuman (Human Socrates))"), t));
+        //assert!(!validate_atom(&space, &atom("(= SocratesIsHuman (Human Time))")));
+    }
+
+    #[test]
     fn arrow_allows_undefined_type() {
         init_logger();
         let space = metta_space("
