dd44c7cc595b6e38e637ff0dad4d210737ed6801
Vitaly Bogdanov <vsbogd@gmail.com>
2022-04-18 12:53:57 +0300

    Return Python atoms in Python wrapper of GroundingSpace.query

--- python/tests/test_grounding_space.py
+++ python/tests/test_grounding_space.py
@@ -31,3 +31,11 @@ class GroundingSpaceTest(unittest.TestCase):
         self.assertTrue(kb.replace_atom(S("b"), S("d")))
 
         self.assertEqual(kb.get_atoms(), [S("a"), S("d"), S("c")])
+
+    def test_complex_query(self):
+        kb = GroundingSpace()
+        kb.add_atom(E(S("A"), S("B")))
+        kb.add_atom(E(S("C"), S("B")))
+
+        result = kb.query(E(S(","), E(S("A"), V("x")), E(S("C"), V("x"))))
+        self.assertEqual(result, [{"x": S("B")}])
