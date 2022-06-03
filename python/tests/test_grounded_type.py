5b831a98dd811116789c5d3b07a44dcd7d911469
Vitaly Bogdanov <vsbogd@gmail.com>
2022-06-03 18:21:20 +0300

    Add unit test to check the default type of the OperationAtom
    Skip unit test with TODO comment because the behaviour is to be defined.

--- python/tests/test_grounded_type.py
+++ python/tests/test_grounded_type.py
@@ -31,3 +31,16 @@ class GroundedTypeTest(unittest.TestCase):
             metta.interpret("(> 1 1)")[0].get_grounded_type(),
             metta.interpret("(+ 1 1)")[0].get_grounded_type())
 
+    # TODO: For now OperationAtom without type has Undefined type. We discussed
+    # that it would be nice to have something like *args in the future. The
+    # type of untyped operations is yet another example showing this can be
+    # useful. So, the type of untyped operations might become something like
+    # (-> *Undefined Undefined) in the future.
+    @unittest.skip("Behavior to be defined")
+    def test_undefined_operation_type(self):
+        metta = MeTTa()
+        metta.add_atom("untyped", ValueAtom(None))
+        metta.add_atom("untop", OperationAtom("untop", lambda: None))
+        self.assertNotEqual(metta.parse_single("untop").get_grounded_type(),
+                metta.parse_single("untyped").get_grounded_type())
+
