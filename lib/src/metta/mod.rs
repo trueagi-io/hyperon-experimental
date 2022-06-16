219b2fa7e3624cdbfbc4b08ba6a24192c9d66e4b
Vitaly Bogdanov <vsbogd@gmail.com>
2022-06-16 17:07:00 +0300

    Add elements of the common library in Rust

--- lib/src/metta/mod.rs
+++ lib/src/metta/mod.rs
@@ -7,15 +7,31 @@ mod examples;
 use crate::Atom;
 use crate::space::grounding::GroundingSpace;
 use text::{SExprParser, Tokenizer, SExprSpace};
+use crate::common::*;
+use regex::Regex;
 
 pub fn metta_space(text: &str) -> GroundingSpace {
     let mut parser = SExprSpace::new();
+    parser.register_token(Regex::new(r"\d+").unwrap(),
+        |n| Atom::value(n.parse::<i32>().unwrap()));
+    parser.register_token(Regex::new(r"true|false").unwrap(),
+        |b| Atom::value(b.parse::<bool>().unwrap()));
+    parser.register_token(Regex::new(r"<").unwrap(), |_| Atom::gnd(LT));
     parser.add_str(text).unwrap();
     GroundingSpace::from(&parser)
 }
 
+fn common_tokenizer() -> Tokenizer {
+    let mut tokenizer = Tokenizer::new();
+    // FIXME: this code is duplicated in metta_space()
+    tokenizer.register_token(Regex::new(r"\d+").unwrap(),
+        |num| Atom::value(num.parse::<i32>().unwrap()));
+    tokenizer.register_token(Regex::new(r"<").unwrap(), |_| Atom::gnd(LT));
+    tokenizer
+}
+
 pub fn metta_atom(atom: &str) -> Atom {
-    let tokenizer = Tokenizer::new();
+    let tokenizer = common_tokenizer();
     let mut parser = SExprParser::new(atom);
     let atom = parser.parse(&tokenizer);
     if let Some(atom) = atom {
