8948cb2ed4cc0f8e826b0a54345222f29e4d12a8
Aleksei Isaev <103368676+h555yand@users.noreply.github.com>
2022-10-31 12:58:12 +0300

    Update metta.py
    It were no need to commit.

--- python/tests/metta.py
+++ python/tests/metta.py
@@ -7,7 +7,5 @@ if __name__ == "__main__":
     os.system('clear')
     print("\n========= MeTTa version 0.0 =========\n\n")
     metta = MeTTa()
-     #for result in metta.import_file(sys.argv[1]):
-    filename = '/home/aleksei/Downloads/hyperon-experimental/python/tests/scripts/test1.metta'
-    for result in metta.import_file(filename):
+    for result in metta.import_file(sys.argv[1]):
         print(result)
