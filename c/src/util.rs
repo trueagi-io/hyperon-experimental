7330ae80cec969a96ba311b2425e2c481ffc17e2
Vitaly Bogdanov <vsbogd@gmail.com>
2022-11-02 01:34:16 +0300

    Add ArcMutex wrapper for C API

--- c/src/util.rs
+++ c/src/util.rs
@@ -2,6 +2,8 @@ use std::ffi::c_void;
 use std::ffi::CString;
 use std::os::raw::c_char;
 use std::ffi::CStr;
+use hyperon::common::ptr::ArcMutex;
+use std::ops::{Deref, DerefMut};
 
 #[repr(C)]
 pub struct array_t<T> {
@@ -33,4 +35,31 @@ pub fn str_as_cstr(s: &str) -> CString {
 
 pub fn string_as_cstr(s: String) -> CString {
     CString::new(s).expect("CString::new failed")
+}
+
+// We cannot use imported ArcMutex in C API because it is not correctly
+// converted int C header and header cannot be compiled. This wrapper just
+// solves the issue by shadowing original type.
+pub struct ArcMutexAdapter<T>(ArcMutex<T>);
+
+impl<T> ArcMutexAdapter<T> {
+    pub fn new(value: T) -> *mut Self {
+        Box::into_raw(Box::new(Self(ArcMutex::new(value))))
+    }
+
+    pub fn drop(ptr: *mut Self) {
+        unsafe { drop(Box::from_raw(ptr)) }
+    }
+
+    pub fn borrow(&self) -> Box<dyn Deref<Target=T> + '_> {
+        self.0.borrow()
+    }
+
+    pub fn borrow_mut(&mut self) -> Box<dyn DerefMut<Target=T> + '_> {
+        self.0.borrow_mut()
+    }
+
+    pub fn arcmutex(&self) -> &ArcMutex<T> {
+        &self.0
+    }
 }
