25ce3f860abd12f81539452e2cf4a41335189e4f
Aleksei Isaev <aleksey.isaev@gmail.com>
2022-11-08 11:53:17 +0300

    Add files via upload
    Expression Atoms for Graph knowledge base.

--- lib/src/child_ai/graph_kb.rs
+++ lib/src/child_ai/graph_kb.rs
@@ -1,6 +1,6 @@
 use crate::*;
 use crate::space::grounding::GroundingSpace;
-use crate::Atom;
+//use crate::Atom;
 use std::fmt::{Display, Debug};
 use std::{fmt, clone};
 use ndarray::Array1;
@@ -11,6 +11,7 @@ pub const MATCH_RATIO: f32 = 0.95;
 static mut NODE_ID:u32 = 0;
 static mut LINK_ID:u32 = 0;
 
+#[derive(PartialEq)]
 pub enum NodeEdge{
     Node,
     Edge,
@@ -59,7 +60,7 @@ impl Grounded for MemoryNode {
 }
 impl fmt::Display for MemoryNode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", &self.Node_ID)
+        write!(f, "{}", &self.Name)
     }
 }
 
@@ -69,6 +70,9 @@ pub struct LinksEdge {
     Source_Node_ID: u32,
     Dist_Node_ID: u32,
     Weights: Vec<f32>,
+    Name: String,
+    Source_Name: String,
+    Dist_Name: String,
 }
 impl LinksEdge {
     /// Constructs new empty node.
@@ -78,6 +82,9 @@ impl LinksEdge {
             Source_Node_ID: 0,
             Dist_Node_ID: 0,
             Weights: Vec::new(),
+            Name: String::from(""),
+            Source_Name: String::from(""),
+            Dist_Name: String::from(""),
         }
     }
     pub fn node_edge_() -> NodeEdge {
@@ -97,7 +104,7 @@ impl Grounded for LinksEdge {
 }
 impl fmt::Display for LinksEdge {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", &self.Edge_ID)
+        write!(f, "{}", &self.Name)
     }
 }
 
@@ -181,7 +188,7 @@ pub fn identify_code_in_memory(memory_space: & mut GroundingSpace, new_node_atom
     }
 }
 
-pub fn set_link_for_nodes(link_space: &mut GroundingSpace, prev: & Atom, next: & Atom) -> Vec<Atom> {
+pub fn set_link_for_nodes(link_space: &mut GroundingSpace, prev: & Atom, next: & Atom, link_name: &String) -> Vec<Atom> {
     let mut vec_atoms = Vec::new(); //first is Link, second is Prev Node, third is Next Node
     let mut temp_atom_prev = (*prev).clone();
     let mut temp_atom_next = (*next).clone();
@@ -229,6 +236,7 @@ pub fn set_link_for_nodes(link_space: &mut GroundingSpace, prev: & Atom, next: &
     Link_new_node.Source_Node_ID = prev.as_gnd::<MemoryNode>().unwrap().Node_ID;
     Link_new_node.Dist_Node_ID = next.as_gnd::<MemoryNode>().unwrap().Node_ID;
     Link_new_node.Weights= vec![0.01; SEGMENT];
+    Link_new_node.Name = (*link_name).clone();
     let Link_new_node_atom = Atom::gnd(Link_new_node);
     link_space.add(Link_new_node_atom.clone());
 
@@ -266,42 +274,245 @@ pub fn match_ratio(data: &Atom, pattern: &Atom) -> f32 {
     match_ratio
 }
 
+pub fn find_link_by_name(memory_space: & GroundingSpace, link_name: &String) -> Result<Atom, String> {
+    let temp_link = LinksEdge::new();
+    let mut temp_atom = Atom::gnd(temp_link.clone());
+    for next_edge in &(*memory_space.borrow_vec()) {
+        temp_atom = (*next_edge).clone();
+        let temp_type_atom = temp_atom.as_gnd::<LinksEdge>();
+        match temp_type_atom {
+        Some(temp_type_atom)=> {
+            println!("(*link_name) {}", (*link_name));
+            println!("Name {}", temp_atom.as_gnd::<LinksEdge>().unwrap().Name);
+            if temp_atom.as_gnd::<LinksEdge>().unwrap().Name == (*link_name) {
+                return Ok(temp_atom.clone())
+            }
+        }, None => { },
+        }
+    }
+    return Err(String::from("Link not found!"))
 }
 
+pub fn find_node_by_name(memory_space: & GroundingSpace, node_name: &String) -> Result<Atom, String> {
+    let temp_node = MemoryNode::new();
+    let mut temp_atom = Atom::gnd(temp_node.clone());
+    for next_node in &(*memory_space.borrow_vec()) {
+        temp_atom = (*next_node).clone();
+        let temp_type_atom = temp_atom.as_gnd::<MemoryNode>();
+        match temp_type_atom {
+        Some(temp_type_atom)=> {
+            if temp_atom.as_gnd::<MemoryNode>().unwrap().Name == (*node_name) {
+                return Ok(temp_atom.clone())
+            }
+        }, None => { },
+        }
+    }
+    return Err(String::from("Node not found!"))
+}
 
+pub fn parse_link_node(input_atom: &Atom, input_type: NodeEdge) -> Result<Vec<Atom>, String> {
+    if let Atom::Expression(expr_atom) = input_atom {
+        let exr_atom_vec = expr_atom.children();
+        if exr_atom_vec.len() == 2 {
+            let atom_type = exr_atom_vec.get(0).unwrap();
+            println!("atom_type {}", atom_type);
+            if (atom_type.to_string() == "LinksEdge" && input_type == NodeEdge::Edge) || (atom_type.to_string() == "MemoryNode" && input_type == NodeEdge::Node) {
+                let atom_link_node = exr_atom_vec.get(1).unwrap();
+                println!("atom_link_node {}", atom_link_node);
+                let mut vec_atoms = Vec::new();
+                vec_atoms.push((*atom_type).clone());
+                vec_atoms.push((*atom_link_node).clone());
+                return Ok(vec_atoms)
+            }
+        }
+    }
+    return Err(String::from("Atom not Link Node expression!"))
+}
+
+pub fn create_link_nodes_by_names(memory_space: & mut GroundingSpace, input_vec: &Vec<String>) -> Result<Vec<Atom>, String,> {
+    let mut vec_atoms_temp = Vec::new();
+    let mut error_mes = String::from("Could not create Link with Nodes.");
+    if input_vec.len() == 3 {
+        let node1_name = input_vec.get(1).unwrap();
+        let res_node1_find = GraphKB::find_node_by_name(memory_space, &node1_name);
+        match res_node1_find {
+            Ok(node1_atom_find)=>{
+                vec_atoms_temp.push(node1_atom_find);
+            }, Err(error)=>{
+                let mut new_node1 = MemoryNode::new();
+                let code1: Vec<f32> = vec![11.; SEGMENT];
+                new_node1.Name = (*node1_name).clone();
+                new_node1.Key_Code = code1;
+                println!("new_node1.Name {:?}", new_node1.Name);
+                let mut new_atom_node1 = Atom::gnd(new_node1.clone());
+                new_atom_node1 = GraphKB::identify_code_in_memory(memory_space, & new_atom_node1);
+                vec_atoms_temp.push(new_atom_node1.clone());
+                println!("new_atom_node1 {:?}", new_atom_node1);
+                }
+        }
+        let node2_name = input_vec.get(2).unwrap();
+        let res_node2_find = GraphKB::find_node_by_name(memory_space, &node2_name);
+        match res_node2_find {
+            Ok(node2_atom_find)=>{
+                vec_atoms_temp.push(node2_atom_find);
+            }, Err(error)=>{
+                let mut new_node2 = MemoryNode::new();
+                let code2: Vec<f32> = vec![22.; SEGMENT];
+                new_node2.Name = (*node2_name).clone();
+                new_node2.Key_Code = code2;
+                println!("new_node2.Name {:?}", new_node2.Name);
+                let mut new_atom_node2 = Atom::gnd(new_node2.clone());
+                new_atom_node2 = GraphKB::identify_code_in_memory(memory_space, & new_atom_node2);
+                vec_atoms_temp.push(new_atom_node2.clone());
+                println!("new_atom_node2 {:?}", new_atom_node2);
+                }
+        }
+    let link_name = input_vec.get(0).unwrap();
+    let vec_atoms = GraphKB::set_link_for_nodes(memory_space, & vec_atoms_temp.get(0).unwrap(), & vec_atoms_temp.get(1).unwrap(), link_name);
+    if vec_atoms.len() == 3 {
+        println!("new_atom_link {:?}", vec_atoms.get(0).unwrap().as_gnd::<LinksEdge>().unwrap().Name);
+        return Ok(vec_atoms)
+    } else {return Err(error_mes)}
+    }
+    else {return Err(error_mes)}
+}
+
+pub fn parse_graph(memory_space: & GroundingSpace, input_atom: &Atom, output_vec: &mut Vec<String>) -> Result<Vec<Atom>, String,> {
+    let mut vec_atoms_temp = Vec::new();
+    let mut error_mes = String::from("No Link with nodes found! Please use create_link_nodes_by_names() function.");
+    if let Atom::Expression(expr_atom) = input_atom {
+        println!("expr_atom {:?}", expr_atom);
+        let expr_atom_vec = expr_atom.children();
+        if expr_atom_vec.len() == 3 {
+            let res_link = GraphKB::parse_link_node(expr_atom_vec.get(0).unwrap(),NodeEdge::Edge);
+            match res_link {
+                Ok(link_atom_vec)=>{ 
+                    let link_name = (link_atom_vec.get(1).unwrap()).to_string();
+                    output_vec.push(link_name.clone());
+                    let res_link_find = GraphKB::find_link_by_name(memory_space, &link_name);
+                    match res_link_find {
+                        Ok(link_atom_find)=>{ 
+                            vec_atoms_temp.push(link_atom_find.clone());
+                        }, Err(error)=>{}
+                    }
+                }, Err(error)=>{
+                    error_mes = String::from("Link Expression is not valid.");
+                    return Err(error_mes)
+                }
+            }
+            let res_node1 = GraphKB::parse_link_node(expr_atom_vec.get(1).unwrap(),NodeEdge::Node);
+            match res_node1 {
+                Ok(node1_atom_vec)=>{
+                    let node1_name = (node1_atom_vec.get(1).unwrap()).to_string();
+                    output_vec.push(node1_name.clone());
+                    let res_node1_find = GraphKB::find_node_by_name(memory_space, &node1_name);
+                    match res_node1_find {
+                        Ok(node1_atom_find)=>{ 
+                            vec_atoms_temp.push(node1_atom_find.clone());
+                        }, Err(error)=>{}
+                    }
+                    }, Err(error)=>{
+                        error_mes = String::from("Node1 Expression is not valid.");
+                        return Err(error_mes)}
+                }
+            let res_node2 = GraphKB::parse_link_node(expr_atom_vec.get(2).unwrap(),NodeEdge::Node);
+            match res_node2 {
+                Ok(node2_atom_vec)=>{
+                    let node2_name = (node2_atom_vec.get(1).unwrap()).to_string();
+                    output_vec.push(node2_name.clone());
+                    let res_node2_find = GraphKB::find_node_by_name(memory_space, &node2_name);
+                    match res_node2_find {
+                        Ok(node2_atom_find)=>{ 
+                            vec_atoms_temp.push(node2_atom_find.clone());
+                            
+                        }, Err(error)=>{}
+                    }
+                }, Err(error)=>{
+                    error_mes = String::from("Node2 Expression is not valid.");
+                    return Err(error_mes)}
+            }
+            if vec_atoms_temp.len() == 3{
+                return Ok(vec_atoms_temp) 
+            }
+        } else {error_mes = String::from("This is not Link with Nodes Atom.");}
+    } else {error_mes = String::from("This is not Expression Atom.");}
+    return Err(error_mes)
+    }
+
+}
 
 #[cfg(test)]
 mod tests {
- 
-use crate::space::grounding::GroundingSpace;
-use crate::child_ai::graph_kb::*;
-
+use super::*;
+use crate::child_ai::graph_kb::GraphKB;
+use crate::metta::interpreter;
+use crate::metta::text::*;
     #[test]
     pub fn test_save_node_edge(){
+        // Create test Space, Grounded Atoms for Link, Nodes. Save Atoms in Space and set Link.
         let mut Memory_space = GroundingSpace::new();
-
-  
-        let code1: Vec<f32> = vec![0.1; SEGMENT];
+        let code1: Vec<f32> = vec![1.; SEGMENT];
         let mut new_node = MemoryNode::new();
-        new_node.Name = String::from("Name1");
+        new_node.Name = String::from("Node1");
         new_node.Key_Code = code1;
+        println!("new_node {}", new_node.type_());
         let mut new_node_atom = Atom::gnd(new_node.clone());
         let mut result_atom = GraphKB::identify_code_in_memory(&mut Memory_space, & new_node_atom);
         let result_atom1 = result_atom.clone();
-    
-    
-        let code2: Vec<f32> = vec![0.2; SEGMENT];
-        new_node.Name = String::from("Name2");
+        let code2: Vec<f32> = vec![2.; SEGMENT];
+        new_node.Name = String::from("Node2");
         new_node.Key_Code = code2;
         new_node_atom = Atom::gnd(new_node.clone());
         result_atom = GraphKB::identify_code_in_memory(&mut Memory_space, & new_node_atom);
         let result_atom2 = result_atom.clone();
-    
-    
         let mut vec_atoms = Vec::new();
-        vec_atoms = GraphKB::set_link_for_nodes(&mut Memory_space, & result_atom1, & result_atom2);
-        vec_atoms = GraphKB::set_link_for_nodes(&mut Memory_space, vec_atoms.get(1).unwrap(), vec_atoms.get(1).unwrap());
+        let link_name = result_atom1.as_gnd::<MemoryNode>().unwrap().Name.clone() +(&result_atom2.as_gnd::<MemoryNode>().unwrap().Name);
+        vec_atoms = GraphKB::set_link_for_nodes(&mut Memory_space, & result_atom1, & result_atom2, &link_name);
+        let mut link_atom = vec_atoms.get_mut(0).unwrap();
+        let old_atom = link_atom.clone();
+        (*link_atom).as_gnd_mut::<LinksEdge>().unwrap().Name = String::from("Link1");
+        Memory_space.replace(&old_atom, (*link_atom).clone());
+
+
+        // Test for "Expression Atoms for Graph knowledge base" Pull Request.
+        let mut space = GroundingSpace::new();
+        space = Memory_space.clone();
+
+        // test case when Grounded Atoms (Link1, Node1, Node2) exist.
+        //let program = "((LinksEdge Link1) (MemoryNode Node1) (MemoryNode Node2))";
+        // test case when Grounded Atoms (Link11, Node11, Node22) don't exist.
+        let program = "((LinksEdge Link11) (MemoryNode Node11) (MemoryNode Node22))";
+
+        let parse_res = parse_atoms(program);
+        let parse_atom = parse_res.get(0).unwrap();
+
+        let mut names_vec = Vec::new();
+        let res_graph = GraphKB::parse_graph(&space, &parse_atom, &mut names_vec); 
+        match res_graph {
+            Ok(vec_atom_graph)=>{ 
+                let expr_atom_graph = Atom::expr(vec_atom_graph.clone());
+                space.add(expr_atom_graph.clone());
+                println!("Expression Grounded Atom Found and added to Space: {:?}", expr_atom_graph);
+            },
+            Err(error_graph)=>{
+                println!("parse_graph message: {:?}", error_graph);
+                let res_vec_atoms =  GraphKB::create_link_nodes_by_names(& mut space, &names_vec);
+                match res_vec_atoms {
+                    Ok(vec_atom_graph)=>{
+                        let expr_atom_graph = Atom::expr(vec_atom_graph.clone());
+                        space.add(expr_atom_graph.clone());
+                        println!("Expression Grounded Atom Created and added to Space: {:?}", expr_atom_graph);
+                    }, Err(error_graph)=>{
+                        println!("create_link_nodes_by_names: {:?}", error_graph);
+                    }
+                }
+            }
+        }
         
+
+        // Test findning linked nodes.
+        let link_name = String::from("link_name");
+        vec_atoms = GraphKB::set_link_for_nodes(&mut Memory_space, vec_atoms.get(1).unwrap(), vec_atoms.get(1).unwrap(), &link_name);
         let result_atom_find = vec_atoms.get(1).unwrap();
         let node_id = result_atom_find.as_gnd::<MemoryNode>().unwrap().Node_ID;
         let vec_link_ids =  result_atom_find.as_gnd::<MemoryNode>().unwrap().Link_IDs.clone();
@@ -313,4 +524,28 @@ use crate::child_ai::graph_kb::*;
         println!("dist_id {}", dist_id);
     
     }
+
+    // function for parsing testing from text.rs, analog of similar in Python
+    fn parse_atoms(program: &str) -> Vec<Atom> {
+        let tokenizer = Tokenizer::new();
+        let mut parser = SExprParser::new(program);
+        let mut result = Vec::new();
+        while let Some(atom) = parser.parse(&tokenizer) {
+            result.push(atom.clone());
+            println!("parse_atoms {}", atom);
+        }
+        result
+    }
+
+    fn get_atom_type(atom: &Atom)->String{
+        match atom {
+            Atom::Expression(atom)=>{return String::from("Expression")},
+            Atom::Symbol(atom)=>{return String::from("Symbol")},
+            Atom::Variable(atom)=>{return String::from("Variable")},
+            Atom::Grounded(atom)=>{return String::from("Grounded")},
+            _=>{return String::from("Other")}
+        }
+    }
+    
+
 }
