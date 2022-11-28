dd802c45ebc0f7eb3d384365be111d38c7f024ac
Alexey Potapov <pas.aicv@gmail.com>
2022-11-28 23:06:10 +0300

    Add a decorator to pass metta to token registration functions

--- python/hyperon/ext.py
+++ python/hyperon/ext.py
@@ -1,11 +1,18 @@
+def pass_metta(func):
+    def metta_register(metta):
+        func(metta, pass_metta=True)
+    return metta_register
+
 def register_atoms(func):
-    def metta_register_atoms(metta):
-        for rex, atom in func().items():
+    def metta_register(metta, pass_metta=False):
+        regs = func(metta) if pass_metta else func()
+        for rex, atom in regs.items():
             metta.register_atom(rex, atom)
-    return metta_register_atoms
+    return metta_register
 
 def register_tokens(func):
-    def metta_register_tokens(metta):
-        for rex, atom in func().items():
+    def metta_register(metta, pass_metta=False):
+        regs = func(metta) if pass_metta else func()
+        for rex, atom in regs.items():
             metta.register_token(rex, atom)
-    return metta_register_tokens
+    return metta_register
--- python/tests/extention.py
+++ python/tests/extention.py
@@ -1,5 +1,5 @@
 from hyperon.atoms import OperationAtom, ValueAtom
-from hyperon.ext import register_atoms
+from hyperon.ext import *
 
 @register_atoms
 def my_dict_atoms():
@@ -7,3 +7,10 @@ def my_dict_atoms():
         '&my-dict': ValueAtom({'A': 5, 6: 'B'}),
         'get-by-key': OperationAtom('get-by-key', lambda d, k: d[k])
         }
+
+@pass_metta
+@register_tokens
+def my_get_runner(metta):
+    return {
+        '&runner': lambda _: ValueAtom(metta)
+    }
