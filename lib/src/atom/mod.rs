6dba78674d0a43e5782c860fd6d3bd9ea24bdc86
Aleksei Isaev <aleksey.isaev@gmail.com>
2022-12-26 15:28:52 +0300

    Update lib/src/atom/mod.rs
    Co-authored-by: Vitaly Bogdanov <vsbogd@gmail.com>

--- lib/src/atom/mod.rs
+++ lib/src/atom/mod.rs
@@ -219,7 +219,7 @@ impl VariableAtom {
         Self{ name: name.into(), id: 0 }
     }
     
-    // Constructs new variable using `name` and 'id' provided.
+    /// Constructs new variable using `name` and 'id' provided.
     pub fn new_id<T: Into<String>>(name: T, id: usize) -> Self {
         Self{ name: name.into(), id: id }
     }
