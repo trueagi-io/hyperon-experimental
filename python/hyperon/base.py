b32571309be00b1cb4a130546d30d13ab145bcff
Aleksei Isaev <aleksey.isaev@gmail.com>
2022-12-30 15:29:09 +0300

    Update python/hyperon/base.py
    Co-authored-by: Vitaly Bogdanov <vsbogd@gmail.com>

--- python/hyperon/base.py
+++ python/hyperon/base.py
@@ -36,10 +36,7 @@ class GroundingSpace:
 
     def query(self, pattern):
         result = hp.grounding_space_query(self.cspace, pattern.catom)
-        print('query result: ', result)
-        ret_res = [{k: Atom._from_catom(v) for k, v in bindings.items()} for bindings in result]
-        print('query ret_res: ', ret_res)
-        return ret_res
+        return [{k: Atom._from_catom(v) for k, v in bindings.items()} for bindings in result]
 
     def subst(self, pattern, templ):
         return [Atom._from_catom(catom) for catom in
