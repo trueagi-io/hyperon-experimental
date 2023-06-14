6de6a6d8aeb1d9eefbbdc8322d02210cc4eaec83
Vitaly Bogdanov <vsbogd@gmail.com>
2023-06-14 20:39:41 +0300

    Remove "coverage" command which doesn't work

--- python/setup.py
+++ python/setup.py
@@ -1,25 +1,4 @@
-import shlex
-from setuptools import setup, Command
-
-class CoverageCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import sys, subprocess
-
-        result = subprocess.run(
-            shlex.split("coverage run --source hyperon -m unittest discover"),
-            check=False,
-            cwd="./tests",
-        )
-        subprocess.run(shlex.split("coverage html"), check=True, cwd="./tests")
-        raise SystemExit(result.returncode)
+from setuptools import setup
 
 try:
 
@@ -35,7 +14,6 @@ except ImportError:
 
 setup(
     cmdclass={
-        "coverage": CoverageCommand,
         "bdist_wheel": bdist_wheel,
     },
 )
