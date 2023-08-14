d9366962f4d58f6fbf02d94493ddb597e5e96a9f
Luke Peterson <luke@infoplatter.com>
2023-08-14 22:06:53 +0900

    Updating comment

--- c/src/lib.rs
+++ c/src/lib.rs
@@ -9,7 +9,8 @@ pub mod metta;
 /// @ingroup misc_group
 /// @note This function should be called once, prior to any other calls to Hyperon C functions
 ///
-//TODO: Is there a way we can get rid of this function?  Look up the github issue related to this function to remind myself of the latest state
+//TODO: Is there a way we can get rid of this function in the external API?
+// Discussion about alternative ways to init the logger here: https://github.com/trueagi-io/hyperon-experimental/pull/314
 #[no_mangle]
 pub extern "C" fn init_logger() {
    hyperon::common::init_logger(false);
