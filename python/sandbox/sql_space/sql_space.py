fbae8af2e04666549fe15d6dd9796d2d5d6ba212
sveta <bessveta>
2023-09-24 13:13:22 +0300

    add insert

--- python/sandbox/sql_space/sql_space.py
+++ python/sandbox/sql_space/sql_space.py
@@ -20,35 +20,117 @@ def results2bindings(vars, values):
 
 class SqlHelper:
     colums_word = "ColumnNames"
+    insert_command_sql = "INSERT INTO"
 
-    def select(self, space, fields, table, condition="", limit=""):
+    @staticmethod
+    def get_query_atoms(query_atom):
+        children = query_atom.get_children()
+        new_query_atoms = []
+        for ch in children:
+            if 'limit' not in repr(ch).lower():
+                new_query_atoms.append(ch)
+        return new_query_atoms
+
+    @staticmethod
+    def get_fields_and_conditions(query_atom):
+        ''' parse sql query and get columns to select and conditions for filtering '''
+        atoms = query_atom.get_children()
+        fields = {}
+        conditions = {}
+        limit = ""
+        vars_map = {}
+        for atom in atoms:
+            if isinstance(atom, ExpressionAtom):
+                items = atom.get_children()
+                if len(items) == 3:
+                    id_fields = items[1].get_children()
+                    current_field_info = items[2].get_children()
+                    if len(id_fields) != 2 or len(current_field_info) != 2:
+                        raise SyntaxError("Incorrect number of arguments")
+                    # (musicbrainz.artist (id $id) (name $name))
+                    # identification field
+                    id_name = repr(id_fields[0])
+                    vars_map[id_name] = repr(id_fields[1])
+                    # field to select
+                    field_name = repr(current_field_info[0])
+                    vars_map[field_name] = repr(current_field_info[1])
+                    # table
+                    table = repr(items[0])
+                    if table not in fields:
+                        fields[table] = set()
+                    if table not in conditions:
+                        conditions[table] = set()
+                    # add id field to corresponding category (filed/condition)
+                    if isinstance(id_fields[1], VariableAtom):
+                        fields[table].add(id_name)
+                    else:
+                        conditions[table].add(id_name)
+                    # add selected field to corresponding category (filed/condition)
+                    if isinstance(current_field_info[1], VariableAtom):
+                        fields[table].add(field_name)
+                    else:
+                        conditions[table].add(field_name)
+
+                if len(items) == 2 and ("limit" in repr(items[0]).lower()):
+                    limit = repr(items[1])
+        return fields, conditions, limit, vars_map
+
+    @staticmethod
+    def get_fields_and_values(query_atom):
+        ''' parse sql query and get columns to select and conditions for filtering '''
+        atoms = query_atom.get_children()
+        fields = {}
+        vars_map = {}
+        for atom in atoms:
+            if isinstance(atom, ExpressionAtom):
+                items = atom.get_children()
+                if len(items) == 2:
+                    current_field_info = items[1].get_children()
+                    if len(current_field_info) != 2:
+                        raise SyntaxError("Incorrect number of arguments")
+                    # (musicbrainz.artist (id $id) (name $name)
+                    # field to select
+                    field_name = repr(current_field_info[0])
+                    vars_map[field_name] = repr(current_field_info[1])
+                    # table
+                    table = repr(items[0])
+                    if table not in fields:
+                        fields[table] = set()
+                    # add selected field to corresponding category (filed/condition)
+                    fields[table].add(field_name)
+        return fields, vars_map
+
+    def save_query_result(self, sql_space, space, query_atom):
         # if no fields provided get them from information_schema.columns
-        if "*" in fields:
-            query = ''
-            if '.' in table:
-                table_info = table.split('.')
-                if len(table_info) == 2:
-                    query = f'''SELECT column_name  FROM information_schema.columns  WHERE table_schema = '{table_info[0]}'
-                            AND table_name = '{table_info[1]}' '''
-            else:
-                query = f'''SELECT column_name  FROM information_schema.columns  WHERE table_name = {table} '''
-            fields = space.query(E(S(query)))
-            temp = []
-            for val in fields:
-                temp.extend(repr(field)[1:-1] for field in val.values())
-            fields = " ".join(temp)
-
-        fields = f"{self.colums_word}({fields.replace(',', '')})"
-
-        query = f"SELECT {fields} FROM {table} WHERE {condition}"
-        if str(limit).isdigit():
-            query = query + " LIMIT " + str(limit)
-        res = space.query(E(S(query)))
+        res = sql_space.query(query_atom)
+        variables = {}
+        for val in res:
+            for k, v in val.items():
+                variables["$" + str(k)] = str(v)
+        atoms = self.get_query_atoms(query_atom)
+        new_atoms = []
+        for atom in atoms:
+            if isinstance(atom, ExpressionAtom):
+                temp = repr(atom)
+                for k, v in variables.items():
+                    temp = temp.replace(k, v)
+                new_atoms.append(temp)
+        for atom in new_atoms:
+            space.add_atom(E(S(atom)))
         return res
 
-    def insert(self, space, table, fields, values, returning):
-        query = f'''INSERT INTO {table} ({fields})	VALUES ({values}) RETURNING {returning};'''
-        res = space.query(E(S(query)))
+    def insert(self, space, query_atom):
+        fields, vars_map = SqlHelper.get_fields_and_values(query_atom)
+        for table, field_names in fields.items():
+            values = []
+            for v in field_names:
+                values.append(vars_map[v].replace('"', "") if "(" in vars_map[v] and vars_map[v][-2] == ')'
+                              else vars_map[v].replace('"', "'"))
+            fields_str = ", ".join(list(field_names))
+            values_str = ", ".join(list(values))
+            query = f'''{self.insert_command_sql} {table} ({fields_str}) VALUES ({values_str}) RETURNING 0;'''
+            space.query(E(S(query)))
+        res = []
         return res
 
 
@@ -65,53 +147,80 @@ class SqlSpace(GroundingSpace):
     def from_space(self, cspace):
         self.gspace = GroundingSpaceRef(cspace)
 
-    def get_variables(self, query):
-        left = query.rfind(SqlHelper.colums_word)
-        if left == -1:
-            return ""
-        left = left + len(SqlHelper.colums_word)
-        i = left
-        start = left
-        while i < len(query) and query[i] != ")":
-            if query[i] == "(":
-                start = i + 1
-            i = i + 1
-        return query[start:i] if i < len(query) else ""
+    def construct_query(self, query_atom):
+        fields, conditions, limit, vars_map = SqlHelper.get_fields_and_conditions(query_atom)
+        sql_query = "SELECT"
+
+        vars_names = []
+        for k, values in fields.items():
+            for val in values:
+                sql_query = sql_query + f" {k}.{val},"
+                vars_names.append(vars_map[val])
+        sql_query = sql_query[:-1] + " FROM "
+        for k in fields.keys():
+            sql_query = sql_query + f"{k},"
+
+        sql_condition = " WHERE"
+        for k, values in conditions.items():
+            for val in values:
+                if val in vars_map:
+                    sql_condition = sql_condition + f" {k}.{val} = {vars_map[val]} AND"
+        if len(sql_condition) > 6:
+            sql_query = sql_query[:-1] + sql_condition[:-4]
+        else:
+            sql_query = sql_query[:-1]
+        if len(limit) > 0:
+            sql_query = sql_query + f" LIMIT {limit}"
+        return sql_query, vars_names
+
+    def insert(self, sql_query):
+        try:
+            if len(sql_query) > 6:
+                self.cursor.execute(sql_query)
+                self.conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            raise error
+        return BindingsSet.empty()
 
     def query(self, query_atom):
         try:
-            query = repr(query_atom)
-            vars_names = self.get_variables(query)
-            new_bindings_set = BindingsSet.empty()
-            vars = []
-            if len(vars_names) > 0:
-                vars = vars_names.replace("$", "").split(" ")
-                str_for_query = ", ".join(vars)
-                query = query.replace(SqlHelper.colums_word, "").replace("(" + vars_names + ")", str_for_query)
-
-            self.cursor.execute(query[1:-1])
-            values = self.cursor.fetchall()
-            if len(vars_names) == 0 and len(values) > 0:
-                vars = [f"var{i + 1}" for i in range(len(values[0]))]
-            if len(vars) > 0:
-                return results2bindings(vars, values)
-            if query.strip().startswith("(INSERT"):
-                self.conn.commit()
-            return new_bindings_set
+            atoms = query_atom.get_children()
+            if len(atoms) > 0 and SqlHelper.insert_command_sql in repr(atoms[0]):
+                return self.insert(repr(atoms[0]))
+            else:
+                new_bindings_set = BindingsSet.empty()
+                sql_query, vars_names = self.construct_query(query_atom)
+                if len(sql_query) > 6:
+                    self.cursor.execute(sql_query)
+                    values = self.cursor.fetchall()
+                    if len(vars_names) == 0 and len(values) > 0:
+                        vars = [f"var{i + 1}" for i in range(len(values[0]))]
+                    else:
+                        vars = [v[1:] for v in vars_names]
+                    if len(vars) > 0 and len(values) > 0:
+                        return results2bindings(vars, values)
+                return new_bindings_set
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-            if self.conn is not None:
-                self.conn.close()
 
 
 def wrapsqlop(func):
     def wrapper(*args):
-        if len(args) > 0 and isinstance(args[0], GroundedAtom) and isinstance(args[0].get_object(), SpaceRef):
-            space = args[0].get_object()
-            args = args[1:]
-            a = [repr(arg) if isinstance(arg, SymbolAtom) else arg.get_object().value for arg in args]
-            res = func(space, *a)
-            return [ValueAtom(val) for val in res]
+        if len(args) > 1:
+            if isinstance(args[0], GroundedAtom):
+                space1 = args[0].get_object()
+                if isinstance(space1, SpaceRef):
+                    if isinstance(args[1], GroundedAtom):
+                        space2 = args[1].get_object()
+                        if isinstance(space2, SpaceRef):
+                            args = args[2:]
+                            res = func(space1, space2, *args)
+                            return [ValueAtom(val) for val in res]
+                    else:
+                        args = args[1:]
+                        res = func(space1, *args)
+                        return [ValueAtom(val) for val in res]
         return []
 
     return wrapper
@@ -120,12 +229,12 @@ def wrapsqlop(func):
 @register_atoms
 def sql_space_atoms():
     helper = SqlHelper()
-    newNSpaceAtom = OperationAtom('new-sql-space', lambda database, host, user, password, port: [
+    newSQLSpaceAtom = OperationAtom('new-sql-space', lambda database, host, user, password, port: [
         G(SpaceRef(SqlSpace(database, host, user, password, port)))], unwrap=False)
-    sqlSelect = G(OperationObject('sql.select', wrapsqlop(helper.select), unwrap=False))
+    saveQueryResult = G(OperationObject('sql.save-query-result', wrapsqlop(helper.save_query_result), unwrap=False))
     sqlInsert = G(OperationObject('sql.insert', wrapsqlop(helper.insert), unwrap=False))
     return {
-        r"new-sql-space": newNSpaceAtom,
-        r"sql.select": sqlSelect,
+        r"new-sql-space": newSQLSpaceAtom,
+        r"sql.save-query-result": saveQueryResult,
         r"sql.insert": sqlInsert
     }
