9e051975df1234bb762673c2ea5f6f4393063a74
sveta <bessveta>
2023-12-28 12:24:42 +0300

    move regex to type_tokens()

--- python/hyperon/stdlib.py
+++ python/hyperon/stdlib.py
@@ -1,7 +1,9 @@
-from .atoms import ExpressionAtom, E, GroundedAtom, OperationAtom, ValueAtom, NoReduceError
+import re
+
+from .atoms import ExpressionAtom, E, GroundedAtom, OperationAtom, ValueAtom, NoReduceError, AtomType, MatchableObject, \
+    G, S
 from .base import Tokenizer, SExprParser
 from .ext import register_atoms, register_tokens
-from .regex_objects import RegexMatchableAtom
 
 
 class Char:
@@ -59,6 +61,40 @@ def bool_ops():
         r"not": notAtom
     }
 
+def get_string_value(value) -> str:
+    if not isinstance(value, str):
+        value = repr(value)
+    if len(value) > 2 and ("\"" == value[0]) and ("\"" == value[-1]):
+        return value[1:-1]
+    return value
+
+class RegexMatchableObject(MatchableObject):
+    ''' To match atoms with regular expressions'''
+
+    def __init__(self, content, id=None):
+        super().__init__(content, id)
+
+        self.content = self.content.replace("[[", "(").replace("]]", ")").replace("~", " ")
+
+    def match_text(self, text, regexpr):
+        return re.search(pattern=regexpr, string=text.strip(), flags=re.IGNORECASE)
+
+    def match_(self, atom):
+        pattern = self.content
+        text = get_string_value(atom)
+        text = ' '.join([x.strip() for x in text.split()])
+        if pattern.startswith("regex:"):
+            pattern = get_string_value(pattern[6:])
+            matched = self.match_text(text, pattern)
+            if matched is not None:
+                return [{"matched_pattern": S(pattern)}]
+        return []
+
+def RegexMatchableAtom(value, type_name=None, atom_id=None):
+    return G(RegexMatchableObject(value, atom_id), AtomType.UNDEFINED)
+
+
+
 @register_atoms
 def text_ops():
     """Add text operators
@@ -71,6 +107,7 @@ def text_ops():
     see test_stdlib.py for examples.
 
     """
+
     reprAtom = OperationAtom('repr', lambda a: [ValueAtom(repr(a))],
                              ['Atom', 'String'], unwrap=False)
     parseAtom = OperationAtom('parse', lambda s: [ValueAtom(SExprParser(str(s)[1:-1]).parse(Tokenizer()))],
@@ -94,7 +131,8 @@ def type_tokens():
         r"[-+]?\d+(\.\d+)?e[-+]?\d+": lambda token: ValueAtom(float(token), 'Number'),
         "\"[^\"]*\"": lambda token: ValueAtom(str(token[1:-1]), 'String'),
         "\'[^\']\'": lambda token: ValueAtom(Char(token[1]), 'Char'),
-        r"True|False": lambda token: ValueAtom(token == 'True', 'Bool')
+        r"True|False": lambda token: ValueAtom(token == 'True', 'Bool'),
+        r'regex:"[^\"]*"': lambda token: RegexMatchableAtom(token)
     }
 
 @register_tokens
@@ -138,9 +176,4 @@ def call_atom():
 
     return {
         r"call:[^\s]+": newCallAtom
-    }
-@register_tokens
-def regex_atom():
-    return {
-        r'regex:.*': lambda token: RegexMatchableAtom(token)
     }
