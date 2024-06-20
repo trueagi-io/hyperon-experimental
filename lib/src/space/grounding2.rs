e2b6c6e4646468704e0f50d7abf12b1e60b92681
Vitaly Bogdanov <vsbogd@gmail.com>
2024-06-20 10:01:09 +0300

    Add atom token iterator
    Iterator which returns atoms and expression start/end for index.

--- lib/src/space/grounding2.rs
+++ lib/src/space/grounding2.rs
@@ -101,6 +101,77 @@ impl PartialEq for HashAtom {
     }
 }
 
+#[derive(PartialEq, Debug)]
+enum AtomToken<'a> {
+    Atom(&'a Atom),
+    StartExpr,
+    EndExpr,
+}
+
+#[derive(Default)]
+enum AtomTokenIterState<'a> {
+    Single(&'a Atom),
+    Expression(&'a ExpressionAtom),
+    Iterate(&'a ExpressionAtom, usize, Box<AtomTokenIterState<'a>>),
+    #[default]
+    End,
+}
+
+struct AtomTokenIter<'a> {
+    atom: &'a Atom,
+    state: AtomTokenIterState<'a>,
+}
+
+impl<'a> AtomTokenIter<'a> {
+    fn new(atom: &'a Atom) -> Self {
+        let state = match atom {
+            Atom::Symbol(_) | Atom::Variable(_) | Atom::Grounded(_) =>
+                AtomTokenIterState::Single(atom),
+            Atom::Expression(expr) => 
+                AtomTokenIterState::Expression(expr),
+        };
+        Self{ atom, state }
+    }
+}
+
+impl<'a> Iterator for AtomTokenIter<'a> {
+    type Item = AtomToken<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match std::mem::take(&mut self.state) {
+            AtomTokenIterState::Single(atom) => {
+                self.state = AtomTokenIterState::End;
+                Some(AtomToken::Atom(atom))
+            },
+            AtomTokenIterState::Expression(expr) => {
+                self.state = AtomTokenIterState::Iterate(expr, 0, Box::new(AtomTokenIterState::End));
+                Some(AtomToken::StartExpr)
+            },
+            AtomTokenIterState::Iterate(expr, i, prev) => {
+                let children = expr.children();
+                if i < children.len() {
+                    let atom = unsafe{ children.get_unchecked(i) };
+                    let next_state = AtomTokenIterState::Iterate(expr, i + 1, prev);
+                    match atom {
+                        Atom::Symbol(_) | Atom::Variable(_) | Atom::Grounded(_) => {
+                            self.state = next_state;
+                            Some(AtomToken::Atom(atom))
+                        },
+                        Atom::Expression(e) => {
+                            self.state = AtomTokenIterState::Iterate(e, 0, Box::new(next_state));
+                            Some(AtomToken::StartExpr)
+                        },
+                    }
+                } else {
+                    self.state = *prev;
+                    Some(AtomToken::EndExpr)
+                }
+            },
+            AtomTokenIterState::End => None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -144,5 +215,25 @@ mod test {
     fn atom_storage_insert_expression() {
         let mut storage = AtomStorage::new();
         assert!(storage.insert(&expr!("A" b {Number::Integer(1)})).is_none());
+    }
+
+    #[test]
+    fn atom_token_iter_symbol() {
+        let atom = Atom::sym("sym");
+        let it = AtomTokenIter::new(&atom);
+        let actual: Vec<AtomToken> = it.collect();
+        assert_eq!(vec![AtomToken::Atom(&atom)], actual);
+    }
+
+    #[test]
+    fn atom_token_iter_expr() {
+        let atom = expr!(("sym" var {Number::Integer(42)} ("sym1" var1 {Number::Integer(43)})));
+        let it = AtomTokenIter::new(&atom);
+        let actual: Vec<AtomToken> = it.collect();
+        assert_eq!(vec![AtomToken::StartExpr, AtomToken::Atom(&expr!("sym")),
+            AtomToken::Atom(&expr!(var)), AtomToken::Atom(&expr!({Number::Integer(42)})),
+            AtomToken::StartExpr, AtomToken::Atom(&expr!("sym1")),
+            AtomToken::Atom(&expr!(var1)), AtomToken::Atom(&expr!({Number::Integer(43)})),
+            AtomToken::EndExpr, AtomToken::EndExpr], actual);
     }
 }
