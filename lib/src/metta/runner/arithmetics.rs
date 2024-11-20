3bc743a95378cc604446afff3c215dd19a37a54a
Vitaly Bogdanov <vsbogd@gmail.com>
2024-11-20 17:59:55 +0300

    Display Rust float Number using dot notation

--- lib/src/metta/runner/arithmetics.rs
+++ lib/src/metta/runner/arithmetics.rs
@@ -111,7 +111,7 @@ impl Display for Number {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Integer(n) => write!(f, "{}", n),
-            Self::Float(n) => write!(f, "{}", n),
+            Self::Float(n) => write!(f, "{:?}", n),
         }
     }
 }
