82ed1a1aa93e221e02bfd8595a8d02b47ea24fbb
Innokenty <innokenty@singularitynet.io>
2025-02-10 09:16:56 +0300

    set-random-seed added

--- lib/src/metta/runner/stdlib/random.rs
+++ lib/src/metta/runner/stdlib/random.rs
@@ -1,3 +1,4 @@
+
 use crate::*;
 use crate::metta::*;
 use super::{grounded_op, regex};
@@ -6,7 +7,10 @@ use crate::metta::runner::number::*;
 use crate::metta::runner::bool::*;
 
 use std::fmt::Display;
-use rand::Rng;
+use std::cell::RefCell;
+use rand::{Rng, SeedableRng, rngs::StdRng};
+
+thread_local!(static GLOBAL_RNG: RefCell<StdRng> = RefCell::new(StdRng::from_os_rng()));
 
 //TODO: In the current version of rand it is possible for rust to hang if range end's value is too
 // big. In future releases (0.9+) of rand signature of sample_single will be changed and it will be
@@ -37,8 +41,7 @@ impl CustomExecute for RandomIntOp {
         if range.is_empty() {
             return Err(ExecError::from("Range is empty"));
         }
-        let mut rng = rand::thread_rng();
-        Ok(vec![Atom::gnd(Number::Integer(rng.gen_range(range)))])
+        Ok(vec![Atom::gnd(Number::Integer(GLOBAL_RNG.with(|generator| generator.borrow_mut().random_range(range))))])
     }
 }
 
@@ -66,10 +69,34 @@ impl CustomExecute for RandomFloatOp {
         if range.is_empty() {
             return Err(ExecError::from("Range is empty"));
         }
-        let mut rng = rand::thread_rng();
-        Ok(vec![Atom::gnd(Number::Float(rng.gen_range(range)))])
+        Ok(vec![Atom::gnd(Number::Float(GLOBAL_RNG.with(|generator| generator.borrow_mut().random_range(range))))])
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct SetRandomSeedOp {}
+
+grounded_op!(SetRandomSeedOp, "set-random-seed");
+
+impl Grounded for crate::metta::runner::stdlib::random::SetRandomSeedOp {
+    fn type_(&self) -> Atom {
+        Atom::expr([ARROW_SYMBOL, ATOM_TYPE_NUMBER, UNIT_TYPE])
+    }
+
+    fn as_execute(&self) -> Option<&dyn CustomExecute> {
+        Some(self)
+    }
+}
+
+impl CustomExecute for crate::metta::runner::stdlib::random::SetRandomSeedOp {
+    fn execute(&self, args: &[Atom]) -> Result<Vec<Atom>, ExecError> {
+        let arg_error = || ExecError::from("set-random-seed expects one argument: number (seed)");
+        let seed: i64 = args.get(0).and_then(Number::from_atom).ok_or_else(arg_error)?.into();
+        GLOBAL_RNG.with(|generator| {*generator.borrow_mut() = StdRng::seed_from_u64(seed as u64)});
+        Ok(vec![UNIT_ATOM])
+    }
+}
+
 
 // NOTE: flip is absent in Python intentionally for conversion testing
 #[derive(Clone, PartialEq, Debug)]
@@ -102,6 +129,8 @@ pub fn register_common_tokens(tref: &mut Tokenizer) {
     tref.register_token(regex(r"random-int"), move |_| { random_int_op.clone() });
     let random_float_op = Atom::gnd(RandomFloatOp{});
     tref.register_token(regex(r"random-float"), move |_| { random_float_op.clone() });
+    let set_seed_op = Atom::gnd(SetRandomSeedOp{});
+    tref.register_token(regex(r"set-random-seed"), move |_| { set_seed_op.clone() });
     let flip_op = Atom::gnd(FlipOp{});
     tref.register_token(regex(r"flip"), move |_| { flip_op.clone() });
 }
@@ -117,6 +146,7 @@ mod tests {
         assert_eq!(run_program(&format!("!(random-int 0 0)")), Ok(vec![vec![expr!("Error" ({ RandomIntOp{} } {Number::Integer(0)} {Number::Integer(0)}) "Range is empty")]]));
         assert_eq!(run_program(&format!("!(chain (eval (random-float 0.0 5.0)) $rfloat (and (>= $rfloat 0.0) (< $rfloat 5.0)))")), Ok(vec![vec![expr!({Bool(true)})]]));
         assert_eq!(run_program(&format!("!(random-float 0 -5)")), Ok(vec![vec![expr!("Error" ({ RandomFloatOp{} } {Number::Integer(0)} {Number::Integer(-5)}) "Range is empty")]]));
+        assert_eq!(run_program(&format!("!(set-random-seed 0)")), Ok(vec![vec![UNIT_ATOM]]));
     }
 
     #[test]
@@ -134,5 +164,8 @@ mod tests {
         assert!(range.contains(&res_f64));
         let res = RandomFloatOp{}.execute(&mut vec![expr!({Number::Integer(0)}), expr!({Number::Integer(0)})]);
         assert_eq!(res, Err(ExecError::from("Range is empty")));
+
+        let res = SetRandomSeedOp{}.execute(&mut vec![expr!({Number::Integer(0)})]);
+        assert_eq!(res, Ok(vec![UNIT_ATOM]));
     }
 }
