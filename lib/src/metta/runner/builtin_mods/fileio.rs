ee210d1d38cf1e28f2d61fb1f61d492ba9b33417
DaddyWesker <avenger15@yandex.ru>
2025-05-13 13:31:54 +0300

    fixes after Vitaly's review

--- lib/src/metta/runner/builtin_mods/fileio.rs
+++ lib/src/metta/runner/builtin_mods/fileio.rs
@@ -23,7 +23,12 @@ impl FileHandle {
     fn open(path: &str, options: &str) -> Result<Self, ExecError> {
 
         let mut opened_file = OpenOptions::new();
-        let opened_file= opened_file.read(options.contains("r")).write(options.contains("w")).create(options.contains("c")).append(options.contains("a")).truncate(!options.contains("a"));
+        let opened_file= opened_file
+            .read(options.contains("r"))
+            .write(options.contains("w"))
+            .create(options.contains("c"))
+            .append(options.contains("a"))
+            .truncate(!options.contains("a"));
 
         match opened_file.open(path)
         {
@@ -32,18 +37,22 @@ impl FileHandle {
         }
     }
 
-    fn read_as_str(&self) -> String
+    fn read_as_str(&self) -> Result<String, ExecError>
     {
         self.0.borrow_mut().seek(SeekFrom::Start(0)).unwrap();
         let mut contents = String::new();
-        let _ = self.0.borrow_mut().read_to_string(&mut contents);
-        contents
+        match self.0.borrow_mut().read_to_string(&mut contents) {
+            Ok(_) => Ok(contents),
+            Err(_) => Err(ExecError::from("Failed to read file contents"))
+        }
     }
 
-    fn write(&self, content: &str) -> ()
+    fn write(&self, content: &str) -> Result<(), ExecError>
     {
-        let _ = self.0.borrow_mut().write(&content.as_bytes());
-        ()
+        match self.0.borrow_mut().write(&content.as_bytes()) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(ExecError::from("Failed to read file contents"))
+        }
     }
 }
 
@@ -86,17 +95,17 @@ impl ModuleLoader for FileioModLoader {
         let mut tref = target.tokenizer().borrow_mut();
 
         tref.register_function(GroundedFunctionAtom::new(
-            r"file-open".into(),
+            r"file-open!".into(),
             Atom::expr([ARROW_SYMBOL, ATOM_TYPE_STRING, ATOM_TYPE_STRING, ATOM_TYPE_FILE_HANDLE]),
             file_open));
 
         tref.register_function(GroundedFunctionAtom::new(
-            r"file-read".into(),
+            r"file-read!".into(),
             Atom::expr([ARROW_SYMBOL, ATOM_TYPE_FILE_HANDLE, ATOM_TYPE_STRING]),
             file_read));
 
         tref.register_function(GroundedFunctionAtom::new(
-            r"file-write".into(),
+            r"file-write!".into(),
             Atom::expr([ARROW_SYMBOL, ATOM_TYPE_FILE_HANDLE, ATOM_TYPE_STRING, UNIT_ATOM]),
             file_write));
 
@@ -105,7 +114,7 @@ impl ModuleLoader for FileioModLoader {
 }
 
 fn file_open(args: &[Atom]) -> Result<Vec<Atom>, ExecError> {
-    let arg_error = || ExecError::from("file-open expects filepath and options in form of string atoms as an argument");
+    let arg_error = || ExecError::from("file-open! expects filepath and options in form of string atoms as an argument");
     let filepath = args.get(0).and_then(Str::from_atom).ok_or_else(arg_error)?;
     let options = args.get(1).and_then(Str::from_atom).ok_or_else(arg_error)?;
 
@@ -114,21 +123,21 @@ fn file_open(args: &[Atom]) -> Result<Vec<Atom>, ExecError> {
 }
 
 fn file_read(args: &[Atom]) -> Result<Vec<Atom>, ExecError> {
-    let arg_error = || ExecError::from("file-read expects filehandle as an argument");
+    let arg_error = || ExecError::from("file-read! expects filehandle as an argument");
     let filehandle = args.get(0).ok_or_else(arg_error)?.into();
-    let filehandle = Atom::as_gnd::<FileHandle>(filehandle).ok_or("file-read expects filehandle as an argument")?;
+    let filehandle = Atom::as_gnd::<FileHandle>(filehandle).ok_or("file-read! expects filehandle as an argument")?;
 
     let message = filehandle.read_as_str();
-    Ok(vec![Atom::gnd(Str::from_string(message))])
+    Ok(vec![Atom::gnd(Str::from_string(message?))])
 }
 
 fn file_write(args: &[Atom]) -> Result<Vec<Atom>, ExecError> {
-    let arg_error = || ExecError::from("file-write expects filehandle and content (string atom) as an arguments");
+    let arg_error = || ExecError::from("file-write! expects filehandle and content (string atom) as an arguments");
     let filehandle = args.get(0).ok_or_else(arg_error)?.into();
-    let filehandle = Atom::as_gnd::<FileHandle>(filehandle).ok_or("file-write expects filehandle and content (string atom) as an arguments")?;
+    let filehandle = Atom::as_gnd::<FileHandle>(filehandle).ok_or("file-write! expects filehandle and content (string atom) as an arguments")?;
     let content = args.get(1).and_then(Str::from_atom).ok_or_else(arg_error)?;
 
-    filehandle.write(content.as_str());
+    filehandle.write(content.as_str()).expect("Failed to write file");
     unit_result()
 }
 
@@ -136,21 +145,24 @@ fn file_write(args: &[Atom]) -> Result<Vec<Atom>, ExecError> {
 mod tests {
     use crate::metta::*;
     use crate::metta::runner::run_program;
+    use tempfile::NamedTempFile;
 
     #[test]
     fn test_filehandle() {
-        let program = "
+
+        let file = NamedTempFile::new().unwrap();
+
+        let program = format!("
             !(import! &self fileio)
-            !(bind! &fhandle (file-open \"test.txt\" \"rwc\"))
-            !(file-write &fhandle \"check write/read\")
-            !(assertEqual (file-read &fhandle) \"check write/read\")
-        ";
-        assert_eq!(run_program(program), Ok(vec![
+            !(bind! &fhandle (file-open! \"{}\" \"rwc\"))
+            !(file-write! &fhandle \"check write/read\")
+            !(assertEqual (file-read! &fhandle) \"check write/read\")
+        ", file.path().to_str().unwrap());
+        assert_eq!(run_program(program.as_str()), Ok(vec![
             vec![UNIT_ATOM],
             vec![UNIT_ATOM],
             vec![UNIT_ATOM],
             vec![UNIT_ATOM]
         ]));
-        std::fs::remove_file("test.txt").expect("File not removed");
     }
 }
