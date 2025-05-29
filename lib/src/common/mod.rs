632a58c17099e00c23b5a32ff98962b84ed3a419
Vitaly Bogdanov <vsbogd@gmail.com>
2025-05-29 18:12:04 +0300

    Remove unused GndRefCell structure

--- lib/src/common/mod.rs
+++ lib/src/common/mod.rs
@@ -8,45 +8,13 @@ pub mod multitrie;
 pub mod holeyvec;
 pub mod owned_or_borrowed;
 pub mod vecondemand;
-
 mod flex_ref;
 pub use flex_ref::FlexRef;
 
-use std::cell::RefCell;
-use std::fmt::{Debug, Display};
 use std::collections::HashMap;
 
 #[cfg(test)]
 pub(crate) mod test_utils;
-
-// GndRefCell is used to keep pointer to the data located on heap as GroundedAtom.
-// RefCell itself doesn't implement Display, and forwards PartialEq to internal
-// data even when kept type doesn't implement PartialEq. GndRefCell fixes this
-// by implementing dummy Display and implementing PartialEq via comparing
-// pointers to the data.
-#[derive(Clone, Debug)]
-pub struct GndRefCell<T>(RefCell<T>);
-
-impl<T> GndRefCell<T> {
-    pub const fn new(value: T) -> Self {
-        Self(RefCell::new(value))
-    }
-    pub fn raw(&self) -> &RefCell<T> {
-        &self.0
-    }
-}
-
-impl<T> PartialEq for GndRefCell<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.as_ptr() == other.0.as_ptr()
-    }
-}
-
-impl<T> Display for GndRefCell<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "GndRefCell")
-    }
-}
 
 #[derive(Clone)]
 pub struct CachingMapper<K: Clone + std::hash::Hash + Eq + ?Sized, V: Clone, F: Fn(K) -> V> {
