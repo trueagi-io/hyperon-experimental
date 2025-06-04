8f919e1f04686feb20180d485f86854507d64f94
Vitaly Bogdanov <vsbogd@gmail.com>
2025-06-04 15:25:56 +0300

    Add unit tests written in MeTTa

--- lib/tests/metta.rs
+++ lib/tests/metta.rs
@@ -1,9 +1,10 @@
 use hyperon_common::collections::ImmutableString;
 use hyperon_atom::*;
 use hyperon_atom::gnd::*;
-use hyperon::metta::UNIT_ATOM;
+use hyperon::metta::{UNIT_ATOM, ERROR_SYMBOL};
 use hyperon::metta::text::*;
 use hyperon::metta::runner::{Metta, EnvBuilder};
+use hyperon::metta::runner::str::atom_to_string;
 
 #[test]
 fn test_reduce_higher_order() {
@@ -69,4 +70,42 @@ fn test_method_into_grounded_atom() {
 
     assert_eq!(execute_expr(Atom::expr([Atom::gnd(op)])),
         Ok(vec![vec![Atom::sym("some-symbol")]]));
+}
+
+fn run_metta_test(code: &[u8]) -> Result<(), String> {
+    let metta = Metta::new(Some(EnvBuilder::test_env()));
+    let result = metta.run(SExprParser::new(code));
+    match result {
+        Ok(result) => {
+            for vec in result {
+                for atom in vec {
+                    match <&[Atom]>::try_from(&atom) {
+                        Ok([op, atom, err]) if *op == ERROR_SYMBOL => {
+                            let err = atom_to_string(err);
+                            return Err(format!("{} returns error: {}", atom, err));
+                        },
+                        _ => {},
+                    }
+                }
+            }
+            Ok(())
+        },
+        Err(err) => {
+            Err(err)
+        }
+    }
+}
+
+macro_rules! run_metta_test {
+    ($name:literal) => {
+        match run_metta_test(include_bytes!($name)) {
+            Ok(()) => {},
+            Err(msg) => assert!(false, "Test {} fails: {}", $name, msg),
+        }
+    }
+}
+
+#[test]
+fn test_run_metta() {
+    run_metta_test!("test_stdlib.metta");
 }
