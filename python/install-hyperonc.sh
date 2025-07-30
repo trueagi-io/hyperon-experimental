8c7a316b1ee82abe0019fb199ea988d766fe44d5
Artur Gontijo <arturgontijo@gmail.com>
2025-07-30 15:39:24 -0300

    Try add protobuf to Conan.

--- python/install-hyperonc.sh
+++ python/install-hyperonc.sh
@@ -43,6 +43,21 @@ python3 -m pip install cmake==3.24 conan==2.16.1 pip==23.1.2
 PATH="${PATH}:${HOME}/.local/bin"
 conan profile detect --force
 
+# Create a custom profile with C++17 for protobuf compatibility
+cat > ~/.conan2/profiles/default << EOF
+[settings]
+arch=x86_64
+build_type=Release
+compiler=gcc
+compiler.cppstd=gnu17
+compiler.libcxx=libstdc++
+compiler.version=10
+os=Linux
+
+[conf]
+tools.cmake.cmaketoolchain:generator=Unix Makefiles
+EOF
+
 mkdir -p ${HOME}/hyperonc
 cd ${HOME}/hyperonc
 git init
